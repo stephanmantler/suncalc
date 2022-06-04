fde350f632d7a835e6bca57228df6b4d99a763ba
stephan mantler <step@stephanmantler.com>
2022-06-04 09:48:20 +0000

    fixed param typo

--- suncalc.js
+++ suncalc.js
@@ -22,7 +22,7 @@ const rad  = PI / 180
 
 const dayMs = 1000 * 60 * 60 * 24
 const J1970 = 2440588
-const J2000 = 245154
+const J2000 = 2451545
 
 function toJulian(date) { return date.valueOf() / dayMs - 0.5 + J1970; }
 function fromJulian(j)  { return new Date((j + 0.5 - J1970) * dayMs); }
